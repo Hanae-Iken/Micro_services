2f7d4a18ca576de1990209422443aafba88dce24
Hanae-Iken <hanaeiken572@gmail.com>
2025-03-12 23:31:19 +0000

    properties de account-service

--- account-service/src/main/java/ma/ensat/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/ma/ensat/accountservice/AccountServiceApplication.java
@@ -1,13 +1,46 @@
 package ma.ensat.accountservice;
 
+import ma.ensat.accountservice.entities.BankAccount;
+import ma.ensat.accountservice.enums.AccountType;
+import ma.ensat.accountservice.repository.BankAccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @SpringBootApplication
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(BankAccountRepository accountRepository) {
+        return args -> {
+            BankAccount bankAccount1 = BankAccount.builder()
+                    .accountId(UUID.randomUUID().toString())
+                    .currency("MAD")
+                    .balance(98000)
+                    .createAt(LocalDate.now())
+                    .type(AccountType.CURRENT_ACCOUNT)
+                    .customerId(Long.valueOf(1))
+                    .build();
+            BankAccount bankAccount2 = BankAccount.builder()
+                    .accountId(UUID.randomUUID().toString())
+                    .currency("MAD")
+                    .balance(12000)
+                    .createAt(LocalDate.now())
+                    .type(AccountType.SAVING_ACCOUNT)
+                    .customerId(Long.valueOf(2))
+                    .build();
+            accountRepository.save(bankAccount1);
+            accountRepository.save(bankAccount2);
+        };
     }
 
 }
